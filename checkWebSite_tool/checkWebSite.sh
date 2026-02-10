716f80ee36f60b8b25ea496d22cbab7cce3beb3a
Alex478169 <alessandro.mor@outlook.it>
2026-02-10 13:51:39 +0000

    added website reachable feature

--- checkWebSite_tool/checkWebSite.sh
+++ checkWebSite_tool/checkWebSite.sh
@@ -1,42 +1,39 @@
 #!/usr/bin/bash
 
 webSite=$1;
+echo -e "\n\nControllo del sito web $webSite...";
 
 checkRaggiungibilità() {
-		if ping "$(nslookup $1 | grep "Address" | tail -n 1 | awk "{print $2}")"; then
-			echo "il sito è raggiungibile";
+		if ping -c 1 "$1" > /dev/null 2>&1; then
+			echo "✅ il sito è raggiungibile";
 		else
-			echo "il sito non è raggiungibile";
+			echo "❌ il sito non è raggiungibile";
 		fi
 }
 
 checkCertificato(){
-	if curl -Iv https://www.$1/ 2>&1 | grep "SSL certificate"; then
+	if curl -Iv https://www."$1"/ 2>&1 | grep "SSL certificate"; then
 			echo "";
-			echo "il sito è sicuro";
+			echo "✅ il sito è sicuro";
 	else
-		echo "il certificato non è valido. Non visitare questo sito"
+		echo "❌ il certificato non è valido. Non visitare questo sito"
 	fi
 }
 
-
-
-if command -v nslookup > /dev/null; then
-		checkSitoWeb "$webSite";
+if command -v ping > /dev/null; then
+		checkRaggiungibilità "$webSite";
 	else
 		sudo apt update;
-		sudo apt install dnsutils;
-		which nslookup;
+		sudo apt install iputils-ping;
+		which ping;
 		checkSitoWeb "$webSite";
 fi
 
 if command -v whois > /dev/null; then
 	if command -v curl > /dev/null; then
-			echo "Informazioni sul sito: "
-			echo "";
+			echo -e "Informazioni sul sito: \n"
 			whois "$webSite" | head;
-			echo "";
-			echo "";
+			echo -e "\n";
 			checkCertificato "$webSite";
 		else
 			sudo apt update;
